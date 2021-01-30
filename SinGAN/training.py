c33bcf5a13de0ddf73c4debb35028b72a89dfa47
Venkatesh Kotwade <venkateshkotwade@gmail.com>
2021-01-30 13:20:06 +0530

    dbug

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -211,7 +211,7 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None,deep
             fake = netG(noise.detach(),prev)
             output = netD(fake.detach())
             errD_fake = output.mean()
-            errD_fake.backward(retain_graph=True)
+            if(not deepFreeze): errD_fake.backward(retain_graph=True)
             D_G_z = output.mean().item()
 
             gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
