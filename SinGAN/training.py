da72b43409671790928eee9cb5ebe0c6062de45f
Venkatesh Kotwade <venkateshkotwade@gmail.com>
2021-01-30 23:44:14 +0530

    alpha50

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -218,10 +218,10 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None,deep
             D_G_z = output.mean().item()
 
             gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
-            gradient_penalty.backward()
+            if not deepFreeze: gradient_penalty.backward()
 
             errD = errD_real + errD_fake + gradient_penalty
-            optimizerD.step()
+            if(not deepFreeze): optimizerD.step()
 
         errD2plot.append(errD.detach())
         
@@ -233,7 +233,7 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None,deep
             netG.zero_grad()
             output = netD(fake)
             #D_fake_map = output.detach()
-            errG =  -(output.mean()*0)
+            errG =  -(output.mean())
             errG.backward(retain_graph=True)
             if alpha!=0:
                 loss = nn.MSELoss()
@@ -272,7 +272,7 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,centers=None,deep
 
             torch.save(z_opt, '%s/z_opt.pth' % (opt.outf))
 
-        schedulerD.step()
+        if(not deepFreeze): schedulerD.step()
         schedulerG.step()
 
     functions.save_networks(netG,netD,z_opt,opt)
