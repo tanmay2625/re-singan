16b51cae5c1f574f6f5d6bd396f3425c7ff951cc
Venkatesh Kotwade <venkateshkotwade@gmail.com>
2021-03-20 19:55:22 +0530

    added script

--- script.sh
+++ script.sh
@@ -1,15 +1,18 @@
-time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 143_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 0 --threshold 128 
+time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 140_c.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 0 --threshold 128 
 git add .
-git commit -m "run1"
-time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 194_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
+git commit -m "run on 140_c.png"
+time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 86_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 0 --threshold 128 
 git add .
-git commit -m "run2"
-time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 86_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
-git add .
-git commit -m "run3"
-time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 99_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
-git add .
-git commit -m "run4"
-time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 24_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
-git add .
-git commit -m "run5"
+git commit -m "run on 86_input.png"
+# time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 194_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
+# git add .
+# git commit -m "run2"
+# time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 86_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
+# git add .
+# git commit -m "run3"
+# time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 99_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
+# git add .
+# git commit -m "run4"
+# time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 24_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
+# git add .
+# git commit -m "run5"
