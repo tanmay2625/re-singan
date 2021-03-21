295be9e95cd7701f2601c6294ece65dc7c464456
tanmay2625 <53395063+tanmay2625@users.noreply.github.com>
2021-03-21 15:27:47 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
-time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 140.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
+time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 24.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 0 --threshold 128 
 git add .
 git commit -m "run on 140.png"
-#time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 86_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 0 --threshold 128 
-#git add .
-#git commit -m "run on 86_input.png"
-# time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 194_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
-# git add .
-# git commit -m "run2"
+time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 50.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
+git add .
+git commit -m "run on 86_input.png"
+time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 63.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
+git add .
+git commit -m "run2"
 # time python customSR.py --input_dir ./Input/customSR --input_name 11.png --noisy_input_name 86_input.png --sr_factor 2 --ker_size 2 --niter 1000  --lr_g 0.001 --lr_d 0.001 --custom_sr_alpha 20 --frozenWeight 0.7 --nc_im 1 --nc_z 1 --skip_training 1 --threshold 128 
 # git add .
 # git commit -m "run3"
